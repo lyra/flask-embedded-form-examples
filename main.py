f565fcfc3dbc0ef74dac82095708ca13d8698e23
Rafael Pardo Rodriguez <rafael.pardorodriguez@lyra-network.com.br>
2021-07-26 17:14:50 +0000

    MPCOLOMB-262: The project now can show parameters values, it display debuging cards, shows the payment response and calculate signature, and logs the json outputs

--- main.py
+++ main.py
@@ -21,42 +21,21 @@ transactional_parameters.pop('transactionOptions')
 @app.route("/", methods=['GET'])
 def index():
     if len(request.args) != 0:
-        def assign_parameters(obj_dict):
-            for i in obj_dict.keys():
-                if request.args.get(i) != None:
-                    obj_dict[i] = request.args.get(i)
-                if type(obj_dict[i]) == dict:
-                    assign_parameters(obj_dict[i])
-            return obj_dict
+        return render_template('index.html', form=service.assign_parameters(transactional_parameters))
 
-        return render_template('index.html', form=assign_parameters(transactional_parameters))
-
-    return redirect(f"/?amount={transactional_parameters['amount']}&contrib={transactional_parameters['contrib']}&currency={transactional_parameters['currency']}&email={transactional_parameters['customer']['email']}&reference={transactional_parameters['customer']['reference']}&orderId={transactional_parameters['orderId']}")
+    return redirect('/?' + service.url_parser(transactional_parameters))
 
 
 @app.route("/process-data", methods=['POST'])
 def capture_ipn():
-    def new_body_to_send(obj_dict):
-        new_body = {}
-        for i in obj_dict.keys():
-            try:
-                new_body[i] = request.form[i]
-            except KeyError:
-                app.logger.error(f'"{i}" not found or is a key.')
-                pass
-            if type(obj_dict[i]) == dict:
-                new_body[i] = new_body_to_send(obj_dict[i])
-        return new_body
-
-    send_body = new_body_to_send(transactional_parameters)
+    send_body = service.new_body_to_send(transactional_parameters)
     app.logger.info(json.dumps(send_body, indent=4))
     formToken = create_form_token(json.dumps(send_body))
-    kr_public_key = f"{behavior_parameters['shopId']}:{behavior_parameters['sdk_password_testing']}"
 
     return render_template(
         'embedded_form.html', 
         api_url=behavior_parameters['api_url'],
-        kr_public_key=kr_public_key,
+        kr_public_key=behavior_parameters['sdk_password_testing'],
         kr_popin=True if request.form.get('kr-popin') else False,
         formToken=formToken, 
     )
@@ -65,13 +44,14 @@ def capture_ipn():
 @app.route('/ipn', methods=['GET','POST'])
 def ipn():
     if request.form.get('kr-answer') == None:
-        return "Invalid request.", 400
+        return "KO - Invalid request.", 400
     app.logger.info(json.dumps(json.loads(request.form.get('kr-answer').replace('"','\"')), indent=4))
     signature = service.compute_hmac_sha256_signature(behavior_parameters['password_testing'], request.form.get('kr-answer'))
     if signature != request.form.get('kr-hash'):
-        return "Signatures does not match.", 401
+        return "KO - Signatures does not match.", 401
 
-    return "Successful transaction payment.", 200
+    # Check in your DB if the transaction was already processed
+    return "OK - Successful transaction payment.", 200
 
 
 @app.route('/redirect', methods=['POST'])
