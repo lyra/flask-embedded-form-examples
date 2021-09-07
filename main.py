076cb24070c24b2b24566dcc5d8f5e34a33fa26f
Nabil SEMAOUNE <nabil.semaoune@lyra-network.com>
2021-09-07 10:38:25 +0200

    Manage test and production modes.
    ref #PLUGINS-2016

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def index():
 
 
 @app.route("/embedded-form", methods=['POST'])
-def process_ipn():
+def embedded_form():
     api_url = request.form.get('rest_server_api_url') if request.form.get('rest_server_api_url') is None else behavior_parameters['rest_server_api_url']
     send_body = service.new_body_to_send(transactional_parameters)
     CONTRIB = f"Python_Flask_Embedded_Examples_2.x_1.0.0/{flask.__version__}/{sys.version[:5]}"
@@ -43,19 +43,22 @@ def process_ipn():
     return render_template(
         'embedded_form.html', 
         rest_static_url=behavior_parameters['rest_static_url'],
-        kr_public_key=behavior_parameters['test_public_key'],
+        kr_public_key=behavior_parameters['test_public_key'] if behavior_parameters['test_mode'] else behavior_parameters['prod_public_key'],
         kr_popin=True if request.form.get('kr-popin') else False,
         formToken=form_token, 
     )
 
 
 @app.route('/capture-ipn', methods=['GET','POST'])
-def ipn():
+def capture_ipn():
     if request.form.get('kr-answer') == None:
         return "KO - Invalid request.", 400
 
     app.logger.info(json.dumps(json.loads(request.form.get('kr-answer').replace('"', '\"')), indent=4))
-    signature = service.compute_hmac_sha256_signature(behavior_parameters['test_password'], request.form.get('kr-answer'))
+    signature = service.compute_hmac_sha256_signature(
+        behavior_parameters['test_password'] if behavior_parameters['test_mode'] else behavior_parameters['prod_password'],
+        request.form.get('kr-answer')
+    )
     if signature != request.form.get('kr-hash'):
         return "KO - Signatures does not match.", 401
 
@@ -66,13 +69,14 @@ def ipn():
 @app.route('/redirect', methods=['POST'])
 def redirect_():
     """
-    Redirect will proced with the payment, and will either succeed or refused the payment.
+    Redirect will proceed with the payment, and will either succeed or refused the payment.
     """
     if request.args.get('status') == 'success':
         signature = service.compute_hmac_sha256_signature(
-            behavior_parameters['test_hmac_sha_256_key'], request.form.get('kr-answer')
+            behavior_parameters['test_hmac_sha_256_key'] if behavior_parameters['test_mode'] else behavior_parameters['prod_hmac_sha_256_key'],
+            request.form.get('kr-answer')
         )
-        app.logger.info(json.dumps(json.loads(request.form.get('kr-answer').replace('"','\"')),indent=4))
+        app.logger.info(json.dumps(json.loads(request.form.get('kr-answer').replace('"', '\"')),indent=4))
 
         return render_template(
             'redirect.html',
@@ -90,8 +94,8 @@ def create_form_token(entry_body, url=None):
     Create form token to load the payment method
     """
     shop_id = behavior_parameters['shop_id']
-    test_password = behavior_parameters['test_password']
-    string_to_encode = f"{shop_id}:{test_password}"
+    password = behavior_parameters['test_password'] if behavior_parameters['test_mode'] else behavior_parameters['prod_password']
+    string_to_encode = f"{shop_id}:{password}"
 
     if url == None:
         create_payment_url = f"{behavior_parameters['rest_server_api_url']}V4/Charge/CreatePayment"
